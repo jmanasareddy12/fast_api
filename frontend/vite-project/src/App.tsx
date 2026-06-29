0161e99426a43393fe5b16ec9ae155da4a935bb0
jmanasareddy12 <jmanasareddy12@gmail.com>
2026-06-29 16:39:19 +0530

    react update

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,10 +1,16 @@
 import Welcome from './components/welcome';
 import NavBar from './components/NavBar';
+import CompanyCard from './components/CompanyCard';
+import Footer from './components/Footer';
+import JobCard from './components/JobCard';
 function App(){
   return(
     <>
       <NavBar />
       <Welcome />
+      <CompanyCard />
+      <JobCard />
+      <Footer />
     </>
   )
 }

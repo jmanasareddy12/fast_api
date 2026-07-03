64c33cbcf8d4df01d953c413a136b0d39055b96f
jmanasareddy12 <jmanasareddy12@gmail.com>
2026-07-03 16:06:55 +0530

    error correction

--- frontend/vite-project/src/Services/CompanyService.ts
+++ frontend/vite-project/src/Services/CompanyService.ts
@@ -1,28 +1,27 @@
-import axios from "axios";
-import type { Company } from "../types/company";
-
-const API_URL = "http://localhost:8000";
+import api from "./api";
+import type {Company} from "../types/company";
 
 export async function getCompanies(): Promise<Company[]> {
-    const response = await axios.get(`${API_URL}/company`);
+    const response = await api.get("/company/");
     return response.data;
 }
 
-export async function getCompanyById(id: number): Promise<Company> {
-    const response = await axios.get(`${API_URL}/company/${id}`);
+export async function getCompany(id: number): Promise<Company> {
+    const response = await api.get(`/company/${id}`);
     return response.data;
 }
 
 export async function createCompany(company: Company): Promise<Company> {
-    const response = await axios.post(`${API_URL}/company`, company);
+    const response = await api.post("/company/", company);
     return response.data;
 }
 
 export async function updateCompany(id: number, company: Company): Promise<Company> {
-    const response = await axios.put(`${API_URL}/company/${id}`, company);
+    const response = await api.put(`/company/${id}`, company);
     return response.data;
 }
 
 export async function deleteCompany(id: number): Promise<void> {
-    await axios.delete(`${API_URL}/company/${id}`);
+    const response = await api.delete(`/company/${id}`);
+    return response.data;
 }
--- frontend/vite-project/src/Services/JobService.ts
+++ frontend/vite-project/src/Services/JobService.ts
@@ -1,28 +1,27 @@
-import axios from "axios";
-import type { Job } from "../types/job";
+import api from "./api"
+import type { Job } from "../types/job"
 
-const API_URL = "http://localhost:8000";
-
-export async function getJobs(id: number): Promise<Job[]> {
-    const response = await axios.get(`${API_URL}/job/${id}`);
-    return response.data;
+export async function getJobs(): Promise<Job[]> {
+    const response = await api.get("/job/")
+    return response.data
 }
-export async function getJobById(id: number): Promise<Job> {
-    const response = await axios.get(`${API_URL}/job/${id}`);
-    return response.data;
-}   
+
+export async function getJob(id: number): Promise<Job> {
+    const response = await api.get(`/job/${id}`)
+    return response.data
+}
 
 export async function createJob(job: Job): Promise<Job> {
-    const response = await axios.post(`${API_URL}/job`, job );
-    return response.data;
+    const response = await api.post("/job/", job)
+    return response.data
 }
 
-export async function updateJob(id:number, job: Job): Promise<Job> {
-    const response = await axios.put(`${API_URL}/job/${id}`, job);
-    return response.data;
-}  
+export async function updateJob(id: number, job: Job): Promise<Job> {
+    const response = await api.put(`/job/${id}`, job)
+    return response.data
+}
 
-export async function deleteJob(id: number): Promise<Job> {
-    const response = await axios.delete(`${API_URL}/job/${id}`);
-    return response.data;
+export async function deleteJob(id: number): Promise<void> {
+    const response = await api.delete(`/job/${id}`)
+    return response.data
 }

ca2e4236ce814da8c54fba9e10ea498bf1761c04
jmanasareddy12 <jmanasareddy12@gmail.com>
2026-07-03 11:48:30 +0530

    rbac

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,13 +1,15 @@
-import token
+
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from utils.oauth2 import get_current_user, role_required
 from models.users import User
 from schemas.user import UserCreate, UserResponse, Login_User
 from schemas.token import Token
 from database import get_db
 from utils.security import hash_password, verify_password
 from utils.token import create_access_token
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 @router.post("/register", response_model=UserResponse)
@@ -22,21 +24,29 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(db_user)
     return db_user
 
+from fastapi.security import OAuth2PasswordRequestForm
+
 @router.post("/login", response_model=Token)
-def login(user: Login_User, db: Session = Depends(get_db)):
-    existing_user = db.query(User).filter(User.email == user.email).first()
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    existing_user = db.query(User).filter(
+        User.email == form_data.username
+    ).first()
 
     if not existing_user:
-        raise HTTPException(
-            status_code=404,
-            detail="User not found"
-        )
+        raise HTTPException(status_code=404, detail="User not found")
 
-    if not verify_password(user.password, existing_user.hashed_password):
-        raise HTTPException(
-            status_code=401,
-            detail="Incorrect password"
-        )
+    if not verify_password(form_data.password, existing_user.hashed_password):
+        raise HTTPException(status_code=401, detail="Incorrect password")
 
-    access_token=create_access_token(data={"sub": existing_user.id, "role": existing_user.role})
-    return Token(token=access_token, token_type="Bearer")
+    token = create_access_token({
+        "sub": str(existing_user.id),
+        "role": existing_user.role
+    })
+
+    return {
+        "token": token,
+        "token_type": "bearer"
+    }
---
+++ frontend/README.md
@@ -0,0 +1,16 @@
+Architecture
+backend/
+    app/
+     --main.py
+     --database.py
+     models/
+     --users.py
+     --company.py
+     --job.py
+    schemas/
+     --users.py
+     --company.py
+     --job.py
+    routers/
+     --users.py
+     
